a57a55c57b51d3ced6a5f860dee2c48e6ab951c3
Maria Patsyuk <mpatsyuk@mit.edu>
2017-12-07 03:56:06 -0500

    new path for loadlib.C

--- macro/dirc/loadlib.C
+++ macro/dirc/loadlib.C
@@ -1,4 +1,5 @@
 {
   //  gSystem->Load("../../../../sim-recon/master/Linux_Linux-x86_64-gcc4.9.2/plugins/pid_dirc.so");
-  gSystem->Load("../../../../sim-recon/master/Linux_Ubuntu16.04-x86_64-gcc5.4.0/plugins/pid_dirc.so");
+  //gSystem->Load("../../../../sim-recon/master/Linux_Ubuntu16.04-x86_64-gcc5.4.0/plugins/pid_dirc.so");
+  gSystem->Load("../../../../sim-recon/master/Linux_CentOS7-x86_64-gcc4.8.5/plugins/pid_dirc.so");
 }
