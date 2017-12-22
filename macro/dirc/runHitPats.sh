9123a0c428cbfe65c0833f06eed266fccda06882
Maria Patsyuk <mpatsyuk@mit.edu>
2017-12-21 19:12:28 -0500

    small update

--- macro/dirc/runHitPats.sh
+++ macro/dirc/runHitPats.sh
@@ -6,7 +6,7 @@ do
     do
 	echo $i $j
 	#	root -l -q -b loadlib.C drawHitPats1.C\($i-5, $i+5, $j-5, $j+5\)
-	root -l -q -b loadlib.C drawHitPats1.C\($i, $j\);
+	root -l -q -b loadlib.C drawHitPats1.C\($i, $i+10, $j, $j+10\);
     done
 done
 	 
